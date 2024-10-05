1f74e346f59fa8b2116784be5d8f319bbd71ac8d
farzana-shahid <shahidkrorpati@gmail.com>
2024-10-05 15:10:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+var bio = {
+    firstName : "farzana",
+    lastName : "shahid",
+    qualification : "B.sc", 
+    eyeColor : "brawon",
+};
+
+document.getElementById("bio").innerHTML = bio.firstName + " " +  bio.lastName +" "+" and  eyecolor is"+ " " + bio.eyeColor 
+
+
+var icecream = {
+    flavour : "bluebarry",
+    quantity : "2scope",
+    price : 400,
+    
+};
+document.getElementById("ice").innerHTML = icecream.flavour+" "+"icecream "+ " "+icecream.quantity +"  "+ "cost"+" " +icecream.price
+
+var car= {
+    model :"carolla",
+    color : "black",
+    price : 500000,
+
+};
+document.getElementById("car").innerHTML = car.model+" "+"is"+" "+car.color
+
+var student = {
+    firstName : "huzaifa",
+    lastName : "shahid",
+    grade : "A", 
+    class : "five",
+};
+
+document.getElementById("student").innerHTML = student.firstName + " " +  student.lastName +" " + student.grade+" "+"grade"
+
+var market ={
+    veg :"potato",
+    weight : "1kg",
+    price : 200,
+};
+document.getElementById("market").innerHTML=market.veg+" "+market.price+" "+"rupees"+market.weight
+
+var drink={
+    brand : "sprite", 
+    flavour : "mint",
+    quantity:"2cane"
+
+};
+document.getElementById("drink").innerHTML=drink.brand+"  "+drink.flavour+" "+drink.quantity
